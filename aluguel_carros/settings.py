a3588ca49ca5af9012e6f898cbd8984248c9acea
gusgewehr <52550134+gusgewehr@users.noreply.github.com>
2022-08-18 16:52:58 -0300

    postgres

--- aluguel_carros/settings.py
+++ aluguel_carros/settings.py
@@ -80,12 +80,23 @@ WSGI_APPLICATION = 'aluguel_carros.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
+'''DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-}
+}'''
+
+DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'aluguel_carros',
+            'USER': 'aluguel_carros',
+            'PASSWORD': 'admin123',
+            'HOST': 'localhost',
+            'PORT': 5432,
+        }
+    }
 
 AUTHENTICATION_BACKENDS = ( 'django.contrib.auth.models.AnonymousUser', )
 
